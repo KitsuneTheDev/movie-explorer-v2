60643b28cfa6af3fcde1350e6d31bb61f7610bbf
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-05-05 01:42:13 +0300

    update: .env, ApiVariables.jsx, .gitignore
    - ACCESS TOKEN and API KEY relocated in .env file

--- src/constants/ApiVariables.jsx
+++ src/constants/ApiVariables.jsx
@@ -1,5 +1,5 @@
-const API_KEY = `3299c1685aefde4fd8977e8b91f51f50`;
-const ACCESS_TOKEN = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjk5YzE2ODVhZWZkZTRmZDg5NzdlOGI5MWY1MWY1MCIsIm5iZiI6MTc0MDk1MjI2Ni4wMjQ5OTk5LCJzdWIiOiI2N2M0ZDJjYWZkZWQzYjUxNmY5MWQ5YWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.dislR56vU07mCbhYswa-6wREmh-HSSr5gDCRgKBKYCk`;
+const API_KEY = import.meta.env.REACT_APP_API_KEY;
+const ACCESS_TOKEN = import.meta.env.REACT_APP_ACCESS_TOKEN;
 const genreNames = {
     28: "Action",
     12: "Adventure",
